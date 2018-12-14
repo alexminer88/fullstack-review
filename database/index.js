f742613c55c70c019448364a9dea65e867cbfa2d
Alex Miner <alex.miner@gmail.com>
2018-12-14 13:33:16 -0800

    database set up schema and package.json

--- database/index.js
+++ database/index.js
@@ -1,8 +1,21 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://localhost/fetcher'); // is this correct?
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  // repo schema schould keep track of:
+  	// the repo html
+  	// the user
+  	// the number of forks
+  	// "created_at": "2014-03-28T17:55:38Z",
+    // "updated_at": "2016-12-06T13:06:37Z",
+    // "pushed_at": "2016-10-30T13:43:30Z",
+    repoHtml: String,
+    user: String,
+    numForks: String,
+    createdAt: String,
+    updatedAt: String
+
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
